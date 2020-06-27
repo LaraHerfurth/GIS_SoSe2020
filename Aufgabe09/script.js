44762c2920096802487baf63cac0895167bec422
LaraHerfurth <64129649+LaraHerfurth@users.noreply.github.com>
2020-06-27 21:02:10 +0200

    Update script.js

--- Aufgabe09/script.js
+++ Aufgabe09/script.js
@@ -4,7 +4,7 @@ var Aufgabe09;
     document.getElementById("buttonhtml")?.addEventListener("click", handlebuttonhtml);
     document.getElementById("buttonjson")?.addEventListener("click", handlebuttonjson);
     function handlebuttonhtml() {
-        let formData = new FormData(document.forms[1]);
+        let formData = new FormData(document.forms[0]);
         let url = "https://gislaraserver.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
@@ -21,13 +21,13 @@ var Aufgabe09;
         let response = await fetch(_url, { method: "get" });
         let response2 = await response.text();
         let arraySplit = response2.split("angemeldet");
-        document.getElementById("antwort").innerHTML = arraySplit[1];
+        document.getElementById("antwort").innerHTML = arraySplit[0];
     }
     async function communicateJSON(_url) {
         let response = await fetch(_url, { method: "get" });
         let response2 = await response.text();
         let arraySplit = response2.split("angemeldet");
-        let jsonString = JSON.parse(arraySplit[0]);
+        let jsonString = JSON.parse(arraySplit[1]);
         console.log(jsonString);
     }
 })(Aufgabe09 || (Aufgabe09 = {}));
