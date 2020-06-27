a9cc023f44a245f8c04d683bd0840c131ab89ac5
LaraHerfurth <64129649+LaraHerfurth@users.noreply.github.com>
2020-06-27 21:02:45 +0200

    Update script.ts

--- Aufgabe09/script.ts
+++ Aufgabe09/script.ts
@@ -4,7 +4,7 @@ namespace Aufgabe09 {
     document.getElementById("buttonjson")?.addEventListener("click", handlebuttonjson);
 
     function handlebuttonhtml(): void {
-        let formData: FormData = new FormData(document.forms[1]);
+        let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://gislaraserver.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any> formData);
         url = url + "?" + query.toString();
@@ -23,14 +23,14 @@ namespace Aufgabe09 {
         let response: Response = await fetch(_url, { method: "get" });
         let response2: string = await response.text();
         let arraySplit: string[] = response2.split("angemeldet");
-        (<HTMLElement>document.getElementById("antwort")).innerHTML  = arraySplit[1];
+        (<HTMLElement>document.getElementById("antwort")).innerHTML  = arraySplit[0];
       }
 
     async function communicateJSON(_url: RequestInfo): Promise<void> {
         let response: Response = await fetch(_url, { method: "get" });
         let response2: string = await response.text();
         let arraySplit: string[] = response2.split("angemeldet");
-        let jsonString: string = JSON.parse(arraySplit[0]);
+        let jsonString: string = JSON.parse(arraySplit[1]);
         console.log(jsonString);
       }
 
